dfc26052365b12f461df40424511ac6f6993110d
Galileo2010 <15549070600@163.com>
2019-03-22 14:10:39 +0800

    fix some method in ColletionOperator.java

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,22 +22,36 @@ public class CollectionOperator {
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        if (left < right){
+            return IntStream.rangeClosed(left,right).filter(s->s%2==0).boxed().collect(Collectors.toList());
+        }
+        else {
+            return IntStream.rangeClosed(right,left).filter(s->s%2==0).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        }
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream(array).boxed().filter(s->s%2==0).collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        List<Integer> result = Arrays.stream(array).boxed().collect(Collectors.toList());
+        return result.get(result.size()-1);
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        // int[]=>Integer[]=>List<Integer>
+        List<Integer> result = Arrays.stream(firstArray).boxed().collect(Collectors.toList());
+        result.retainAll(Arrays.stream(secondArray).boxed().collect(Collectors.toList()));
+        return  result;
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> first = Arrays.stream(firstArray).collect(Collectors.toList());
+        List<Integer> second = Arrays.stream(secondArray).collect(Collectors.toList());
+        for (Integer item : second) {
+            if (!first.contains(item)) first.add(item);
+        }
+        return first;
     }
 }
